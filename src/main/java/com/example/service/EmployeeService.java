b92f032b9e6fe7b823729485f047eca42ac376e1
Y-Kanekoo <kaneko.yuki2001@gmail.con>
2025-05-21 10:10:38 +0900

    UpdateEmployeeForm 完成

---
+++ src/main/java/com/example/service/EmployeeService.java
@@ -0,0 +1,20 @@
+package com.example.service;
+
+import com.example.domain.Employee;
+import com.example.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Service
+@Transactional
+public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    public List<Employee> showList(){
+        return employeeRepository.findAll();
+    }
+}
