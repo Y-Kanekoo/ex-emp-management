45d6ad5b4d770fbe4d778948df075f4727b53ca7
Y-Kanekoo <kaneko.yuki2001@gmail.con>
2025-05-21 11:48:24 +0900

    javadoc　追加

--- src/main/java/com/example/controller/EmployeeController.java
+++ src/main/java/com/example/controller/EmployeeController.java
@@ -30,5 +30,6 @@ public class EmployeeController {
         return "employee/list";
     }
 
+    @GetMapping
 
 }
--- src/main/java/com/example/service/EmployeeService.java
+++ src/main/java/com/example/service/EmployeeService.java
@@ -16,10 +16,21 @@ public class EmployeeService {
     @Autowired
     private EmployeeRepository employeeRepository;
 
+    /**
+     * 従業員一覧.
+     *
+     * @return すべての従業員一覧
+     */
     public List<Employee> showList(){
         return employeeRepository.findAll();
     }
 
+    /**
+     * 従業員情報を取得する.
+     *
+     * @param id id
+     * @return idで指定された従業員情報
+     */
     public Employee showDetail(Integer id){
 
         return employeeRepository.findById(id);
